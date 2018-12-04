c90294dbf3d6a0ec4b4f978a6fcb3318c1ac5c95
Marcos Trucco <truccomarcos@gmail.com>
2018-12-04 20:09:49 -0300

    test

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PuppeteerSharp;
+using test_pupeteer.Models;
+
+namespace test_pupeteer.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public async Task<IActionResult> Screenshot()
+        {
+            string outputFile = "./screenshots/screen.jpg";
+            await new BrowserFetcher().DownloadAsync(BrowserFetcher.DefaultRevision);
+            var browser = await Puppeteer.LaunchAsync(new LaunchOptions
+            {
+                Headless = true
+            });
+            var page = await browser.NewPageAsync();
+            await page.GoToAsync("http://www.google.com");
+            await page.ScreenshotAsync(outputFile);
+            return Json("hola");
+        }
+
+        public async Task<IActionResult> PDF()
+        {
+            string outputFile = "./screenshots/screen.pdf";
+            //TODO: Implement Realistic Implementation
+            await new BrowserFetcher().DownloadAsync(BrowserFetcher.DefaultRevision);
+            var browser = await Puppeteer.LaunchAsync(new LaunchOptions
+            {
+                Headless = true
+            });
+            var page = await browser.NewPageAsync();
+            await page.GoToAsync("http://www.google.com");
+            await page.PdfAsync(outputFile);
+            return Json("chau");
+        }
+    }
+}
